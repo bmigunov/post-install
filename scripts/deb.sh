aece0a2cf298e7e6b5b6cc1a1f5880f860e93a86
Bogdan Migunov <bogdanmigunov@yandex.ru>
2023-12-30 04:45:55 +0500

    deb.sh: Use common style

--- scripts/deb.sh
+++ scripts/deb.sh
@@ -14,7 +14,7 @@ REMOTE_DEB_TMPDIR=/tmp/remote_deb
 
 function is_package_installed()
 {
-    echo "${FUNCNAME}()" | systemd-cat -p debug -t $0
+    echo "${FUNCNAME}()" | systemd-cat -p debug -t ${0}
 
     sudo dpkg-query -W --showformat='${Status}\n' $@ | \
     grep "install ok installed" &> /dev/null
@@ -23,17 +23,17 @@ function is_package_installed()
 
 function packages_list_apt_install_loop()
 {
-    echo "${FUNCNAME}()" | systemd-cat -p debug -t $0
+    echo "${FUNCNAME}()" | systemd-cat -p debug -t ${0}
 
     for PACKAGE in $(cat "${1}"); do
         if [ $(is_package_installed ${PACKAGE}) != 0 ]; then
             echo "Package '$PACKAGE' is not installed. Installing..." | \
-            systemd-cat -p debug -t $0
+            systemd-cat -p debug -t ${0}
             sudo apt-get -y install ${PACKAGE}
 
             if [[ $? != 0 ]]; then
                 echo "packages_list_apt_install_loop() failure: issue installing package '${PACKAGE}'" | \
-                systemd-cat -p warning -t $0
+                systemd-cat -p warning -t ${0}
                 echo "Issue installing '$PACKAGE'."
             fi
         else
@@ -46,15 +46,15 @@ function deb_packages_install_from_lists()
 {
     PREFIX=
 
-    echo "${FUNCNAME}()" | systemd-cat -p debug -t $0
+    echo "${FUNCNAME}()" | systemd-cat -p debug -t ${0}
 
     if [ ${1} ]; then
         PREFIX="${1}-"
     fi
 
-    COMMON_LIST=$(dirname "$0")"/../data/deb/${PREFIX}common.list"
-    GUI_LIST=$(dirname "$0")"/../data/deb/${PREFIX}gui.list"
-    GAMES_LIST=$(dirname "$0")"/../data/deb/${PREFIX}games.list"
+    COMMON_LIST=$(dirname "${0}")"/../data/deb/${PREFIX}common.list"
+    GUI_LIST=$(dirname "${0}")"/../data/deb/${PREFIX}gui.list"
+    GAMES_LIST=$(dirname "${0}")"/../data/deb/${PREFIX}games.list"
 
     packages_list_apt_install_loop "${COMMON_LIST}"
 
@@ -71,32 +71,32 @@ function remote_deb_packages_install_from_lists()
 {
     PREFIX=
 
-    echo "${FUNCNAME}()" | systemd-cat -p debug -t $0
+    echo "${FUNCNAME}()" | systemd-cat -p debug -t ${0}
     echo "Installing remote packages..."
 
     if [ ${1} ]; then
         PREFIX="${1}-"
     fi
 
-    COMMON_LIST=$(dirname "$0")"/../data/deb/remote/${PREFIX}common.list"
-    GUI_LIST=$(dirname "$0")"/../data/deb/remote/${PREFIX}gui.list"
-    GAMES_LIST=$(dirname "$0")"/../data/deb/remote/${PREFIX}games.list"
+    COMMON_LIST=$(dirname "${0}")"/../data/deb/remote/${PREFIX}common.list"
+    GUI_LIST=$(dirname "${0}")"/../data/deb/remote/${PREFIX}gui.list"
+    GAMES_LIST=$(dirname "${0}")"/../data/deb/remote/${PREFIX}games.list"
 
     mkdir -p -v "${REMOTE_DEB_TMPDIR}"
 
     for REMOTE in $(cat "${COMMON_LIST}"); do
-        wget -q --content-disposition -P "${REMOTE_DEB_TMPDIR}" "${REMOTE}"
+        wget --content-disposition -P "${REMOTE_DEB_TMPDIR}" "${REMOTE}"
     done
 
     if [ ${NO_GUI} = 0 ]; then
         for REMOTE in $(cat "${GUI_LIST}"); do
-            wget -q --content-disposition -P "${REMOTE_DEB_TMPDIR}" "${REMOTE}"
+            wget --content-disposition -P "${REMOTE_DEB_TMPDIR}" "${REMOTE}"
         done
     fi
 
     if [ ${NO_GAMES} = 0 ]; then
         for REMOTE in $(cat "${GAMES_LIST}"); do
-            wget -q --content-disposition -P "${REMOTE_DEB_TMPDIR}" "${REMOTE}"
+            wget --content-disposition -P "${REMOTE_DEB_TMPDIR}" "${REMOTE}"
         done
     fi
 
@@ -108,14 +108,14 @@ function remote_deb_packages_install_from_lists()
 
 function prerequisites_install()
 {
-    echo "${FUNCNAME}()" | systemd-cat -p debug -t $0
+    echo "${FUNCNAME}()" | systemd-cat -p debug -t ${0}
 
-    packages_list_apt_install_loop $(dirname "$0")"/../data/deb/prerequisites.list"
+    packages_list_apt_install_loop $(dirname "${0}")"/../data/deb/prerequisites.list"
 }
 
 function deb_packages_install()
 {
-    echo "${FUNCNAME}()" | systemd-cat -p debug -t $0
+    echo "${FUNCNAME}()" | systemd-cat -p debug -t ${0}
     echo "Installing deb packages..."
 
     deb_packages_install_from_lists ${1}
@@ -124,7 +124,7 @@ function deb_packages_install()
 
 function deb_cleanup()
 {
-    echo "${FUNCNAME}()" | systemd-cat -p debug -t $0
+    echo "${FUNCNAME}()" | systemd-cat -p debug -t ${0}
     echo "deb cleanup..."
 
     sudo apt-get -y autoremove
