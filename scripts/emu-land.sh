f4a1b6c1be857c431019c31f02770eee7f29865a
Bogdan Migunov <bogdanmigunov@yandex.ru>
2023-12-30 04:48:37 +0500

    emu-land.sh: Do not use -q flag in wget

--- scripts/emu-land.sh
+++ scripts/emu-land.sh
@@ -12,8 +12,8 @@
 PS_BIOS_REMOTE="http://www.emu-land.net/consoles/psx/bios?act=getfile&id=4986"
 PS2_BIOS_REMOTE="http://www.emu-land.net/consoles/ps2/bios?act=getfile&id=5017"
 
-PS_BIOS_IMAGES_DIR="${PERSONAL_IMAGES_DIR}/PS/BIOS"
-PS2_BIOS_IMAGES_DIR="${PERSONAL_IMAGES_DIR}/PS2/BIOS"
+PS_BIOS_IMAGES_DIR="${IMG_DIR}/PS/BIOS"
+PS2_BIOS_IMAGES_DIR="${IMG_DIR}/PS2/BIOS"
 
 
 function ps_and_ps2_bios_download()
@@ -21,8 +21,8 @@ function ps_and_ps2_bios_download()
     echo "${FUNCNAME}()" | systemd-cat -p debug -t $0
     echo "Downloading PSOne and PS2 BIOS images..."
 
-    wget -q --content-disposition -P "${PS_BIOS_IMAGES_DIR}" "${PS_BIOS_REMOTE}"
-    wget -q --content-disposition -P "${PS2_BIOS_IMAGES_DIR}" \
+    wget --content-disposition -P "${PS_BIOS_IMAGES_DIR}" "${PS_BIOS_REMOTE}"
+    wget --content-disposition -P "${PS2_BIOS_IMAGES_DIR}" \
          "${PS2_BIOS_REMOTE}"
 }
 
@@ -34,23 +34,23 @@ function emu_land_fetch()
     ps_and_ps2_bios_download
 
     for URI in $(cat $(dirname "${0}")"/../data/emu-land/nes.list"); do
-        wget -q --content-disposition -P "${NES_GAMES_DIR}" "${URI}"
+        wget --content-disposition -P "${NES_GAMES_DIR}" "${URI}"
     done
 
     for URI in $(cat $(dirname "${0}")"/../data/emu-land/sega_mega_drive.list"); do
 
-        wget -q --content-disposition -P "${SEGA_MEGA_DRIVE_GAMES_DIR}" "${URI}"
+        wget --content-disposition -P "${SEGA_MEGA_DRIVE_GAMES_DIR}" "${URI}"
     done
 
     for URI in $(cat $(dirname "${0}")"/../data/emu-land/snes.list"); do
-        wget -q --content-disposition -P "${SNES_GAMES_DIR}" "${URI}"
+        wget --content-disposition -P "${SNES_GAMES_DIR}" "${URI}"
     done
 
     for URI in $(cat $(dirname "${0}")"/../data/emu-land/n64.list"); do
-        wget -q --content-disposition -P "${N64_GAMES_DIR}" "${URI}"
+        wget --content-disposition -P "${N64_GAMES_DIR}" "${URI}"
     done
 
     for URI in $(cat $(dirname "${0}")"/../data/emu-land/zx_spectrum.list"); do
-        wget -q --content-disposition -P "${ZXS_GAMES_DIR}" "${URI}"
+        wget --content-disposition -P "${ZXS_GAMES_DIR}" "${URI}"
     done
 }
