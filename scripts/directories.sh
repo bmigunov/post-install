bca379d23de4d686792530e6befd216f0ea810b7
Bogdan Migunov <bogdanmigunov@yandex.ru>
2023-12-30 04:39:46 +0500

    directories.sh: Update directories

--- scripts/directories.sh
+++ scripts/directories.sh
@@ -9,20 +9,20 @@
 
 
 
-if [ ! ${XDG_STATE_HOME} ]; then
-    export XDG_STATE_HOME="${HOME}/.local/state"
+if [ ! ${XDG_CONFIG_HOME} ]; then
+    export XDG_CONFIG_HOME="${HOME}/.config"
 fi
 
 if [ ! ${XDG_DATA_HOME} ]; then
     export XDG_DATA_HOME="${HOME}/.local/share"
 fi
 
-if [ ! ${XDG_CACHE_HOME} ]; then
-    export XDG_CACHE_HOME="${HOME}/.cache"
+if [ ! ${XDG_STATE_HOME} ]; then
+    export XDG_STATE_HOME="${HOME}/.local/state"
 fi
 
-if [ ! ${XDG_CONFIG_HOME} ]; then
-    export XDG_CONFIG_HOME="${HOME}/.config"
+if [ ! ${XDG_CACHE_HOME} ]; then
+    export XDG_CACHE_HOME="${HOME}/.cache"
 fi
 
 AUDIO_DIR="${HOME}/Audio"
@@ -31,8 +31,7 @@ MUSIC_DIR="${AUDIO_DIR}/Music"
 BOOKS_DIR="${HOME}/Books"
 
 DOCUMENTS_DIR="${HOME}/Documents"
-PERSONAL_DOCUMENTS_DIR="${DOCUMENTS_DIR}/personal"
-JOB_DOCUMENTS_DIR="${DOCUMENTS_DIR}/job"
+DOCUMENTS_JOB_DIR="${DOCUMENTS_DIR}/job"
 
 DOWNLOADS_DIR="${HOME}/Downloads"
 TELEGRAM_DOWNLOADS_DIR="${DOWNLOADS_DIR}/Telegram"
@@ -44,13 +43,12 @@ export NES_GAMES_DIR="${GAMES_DIR}/NES"
 export SEGA_MEGA_DRIVE_GAMES_DIR="${GAMES_DIR}/SEGA_Mega_Drive"
 export SNES_GAMES_DIR="${GAMES_DIR}/SNES"
 export N64_GAMES_DIR="${GAMES_DIR}/N64"
-
-PS_GAMES_DIR="${GAMES_DIR}/PS"
-PS2_GAMES_DIR="${GAMES_DIR}/PS2"
-
 export ZXS_GAMES_DIR="${GAMES_DIR}/ZXS"
 export DOS_GAMES_DIR="${GAMES_DIR}/DOS"
 export WINDOWS_GAMES_DIR="${GAMES_DIR}/WIN"
+
+PS_GAMES_DIR="${GAMES_DIR}/PS"
+PS2_GAMES_DIR="${GAMES_DIR}/PS2"
 
 PICTURES_DIR="${HOME}/Pictures"
 ARTWORK_DIR="${PICTURES_DIR}/artwork"
@@ -70,22 +68,23 @@ MOVIES_DIR="${VIDEO_DIR}/movies"
 SERIES_DIR="${VIDEO_DIR}/series"
 
 WORKSPACE_DIR="${HOME}/workspace"
-JOB_WORKSPACE_DIR="${WORKSPACE_DIR}/job"
+WORKSPACE_JOB_DIR="${WORKSPACE_DIR}/job"
 GHIDRA_DIR="${WORKSPACE_DIR}/ghidra"
-ANDROID_STUDIO_DIR="${WORKSPACE_DIR}/android_studio"
+ANDROID_STUDIO_SDK_DIR="${WORKSPACE_DIR}/android_studio/sdk"
+ANDROID_STUDIO_PROJECTS_DIR="${WORKSPACE_DIR}/android_studio/projects"
 OBSIDIAN_DIR="${WORKSPACE_DIR}/obsidian"
 
-export PERSONAL_SRC_DIR="${WORKSPACE_DIR}/personal/src"
+export SRC_DIR="${WORKSPACE_DIR}/src"
 
-PERSONAL_VAR_DIR="${WORKSPACE_DIR}/personal/var"
-PERSONAL_LOG_DIR="${PERSONAL_VAR_DIR}/log"
+VAR_DIR="${WORKSPACE_DIR}/var"
+SAVED_LOGS_DIR="${VAR_DIR}/log"
 
-export PERSONAL_IMAGES_DIR="${PERSONAL_VAR_DIR}/images"
+export IMG_DIR="${VAR_DIR}/img"
 
-PERSONAL_BACKUP_DIR="${PERSONAL_VAR_DIR}/backup"
-NEOVIM_BACKUP_DIR="${PERSONAL_BACKUP_DIR}/nvim"
+BACKUP_DIR="${VAR_DIR}/backup"
+NEOVIM_BACKUP_DIR="${BACKUP_DIR}/nvim"
 
-export POST_INSTALL_BACKUP_DIR="${PERSONAL_BACKUP_DIR}"/post-install
+export POST_INSTALL_BACKUP_DIR="${BACKUP_DIR}/post-install"
 
 
 LESSHST_DIR="${XDG_STATE_HOME}/less"
@@ -102,10 +101,10 @@ function directories_create()
         ln -s /workspace "${WORKSPACE_DIR}"
     fi
 
-    mkdir -p -v "${AUDIO_DIR}" "${MUSIC_DIR}" "${BOOKS_DIR}" "${DOCUMENTS_DIR} \
-                "${PERSONAL_DOCUMENTS_DIR}" "${JOB_DOCUMENTS_DIR}"             \
-                "${DOWNLOADS_DIR}" "${TELEGRAM_DOWNLOADS_DIR}"                 \
-                "${TORRENT_DOWNLOADS_DIR}" "${GAMES_DIR}" "${NES_GAMES_DIR}"   \
+    mkdir -p -v "${AUDIO_DIR}" "${MUSIC_DIR}" "${BOOKS_DIR}"                   \
+                "${DOCUMENTS_DIR}" "${DOCUMENTS_JOB_DIR}" "${DOWNLOADS_DIR}"   \
+                "${TELEGRAM_DOWNLOADS_DIR}" "${TORRENT_DOWNLOADS_DIR}"         \
+                "${GAMES_DIR}" "${NES_GAMES_DIR}"                              \
                 "${SEGA_MEGA_DRIVE_GAMES_DIR}" "${SNES_GAMES_DIR}"             \
                 "${N64_GAMES_DIR}" "${PS_GAMES_DIR}" "${PS2_GAMES_DIR}"        \
                 "${ZXS_GAMES_DIR}" "${DOS_GAMES_DIR}" "${WINDOWS_GAMES_DIR}"   \
@@ -114,10 +113,11 @@ function directories_create()
                 "${WALLPAPERS_DIR}" "${TORRENTS_DIR}"                          \
                 "${FINISHED_TORRENTS_DIR}" "${VIDEO_DIR}"                      \
                 "${PERSONAL_VIDEO_DIR}" "${MOVIES_DIR}" "${SERIES_DIR}"        \
-                "${WORKSPACE_DIR}" "${JOB_WORKSPACE_DIR}" "${GHIDRA_DIR}"      \
-                "${ANDROID_STUDIO_DIR}" "${OBSIDIAN_DIR}"                      \
-                "${PERSONAL_SRC_DIR}" "${PERSONAL_VAR_DIR}"                    \
-                "${PERSONAL_LOG_DIR}" "${PERSONAL_IMAGES_DIR}"                 \
-                "${PERSONAL_BACKUP_DIR}" "${NEOVIM_BACKUP_DIR}"                \
-                "${POST_INSTALL_BACKUP_DIR}" "${LESSHST_DIR}"
+                "${WORKSPACE_DIR}" "${WORKSPACE_JOB_DIR}" "${GHIDRA_DIR}"      \
+                "${ANDROID_STUDIO_SDK_DIR}" "${ANDROID_STUDIO_PROJECTS_DIR}"   \
+                "${OBSIDIAN_DIR}" "${SRC_DIR}" "${VAR_DIR}"                    \
+                "${SAVED_LOGS_DIR}" "${IMG_DIR}" "${BACKUP_DIR}"               \
+                "${NEOVIM_BACKUP_DIR}" "${POST_INSTALL_BACKUP_DIR}"            \
+                "${LESSHST_DIR}" "${XDG_CONFIG_HOME}" "${XDG_DATA_HOME}"       \
+                "${XDG_STATE_HOME}" "${XDG_CACHE_HOME}"
 }
