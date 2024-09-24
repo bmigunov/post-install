eec9e76f3a31e43ec9109e82a4d44eacb37bb282
Bogdan Migunov <bogdanmigunov@yandex.ru>
2024-09-25 00:40:03 +0500

    Fix flatpak.sh

--- scripts/flatpak.sh
+++ scripts/flatpak.sh
@@ -13,9 +13,9 @@ function flatpak_remotes_add()
 
     REMOTES_LIST=$(dirname "$0")"/../data/flatpak/${PREFIX}/remotes.list"
 
-    for REMOTE in $(cat ${REMOTES_LIST}); do
-        flatpak remote-add --if-not-exists "${REMOTE}"
-    done
+    while read -a REMOTE; do
+        flatpak remote-add --if-not-exists "${REMOTE[0]}" "${REMOTE[1]}"
+    done <"${REMOTES_LIST}"
 }
 
 
@@ -33,13 +33,13 @@ function flatpak_packages_install()
     COMMON_LIST=$(dirname "$0")"/../data/flatpak/${PREFIX}common.list"
     GUI_LIST=$(dirname "$0")"/../data/flatpak/${PREFIX}gui.list"
 
-    for PACKAGE in $(cat ${COMMON_LIST}); do
-        flatpak install "${PACKAGE}"
-    done
+    while read -a PACKAGE; do
+        flatpak install "${PACKAGE[0]}" "${PACKAGE[1]}"
+    done <"${COMMON_LIST}"
 
     if [ ${NO_GUI} = 0 ]; then
-        for PACKAGE in $(cat ${GUI_LIST}); do
-            flatpak install "${PACKAGE}"
-        done
+        while read -a PACKAGE; do
+            flatpak install "${PACKAGE[0]}" "${PACKAGE[1]}"
+        done <"${GUI_LIST}"
     fi
 }
